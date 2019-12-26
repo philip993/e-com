de5a5f41be22dea987ddfffc4081b0b5453eb550
Filip FICO <fitz993m@gmail.com>
2019-12-27 00:38:39 +0100

    basic setup v2

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import "./App.css";
 import Header from "../header/header";
+import Main from "../../pages/main/main";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/pages/main/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Books from "../../components/books/books";
+
+const Main = () => {
+  return (
+    <div>
+      <Books />
+    </div>
+  );
+};
+
+export default Main;
