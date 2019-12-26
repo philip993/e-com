11f704d581478f6a51edb7206025f27f4eb2872c
Filip FICO <fitz993m@gmail.com>
2019-12-26 22:13:04 +0100

    basic setup v1

---
+++ src/components/book/book.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Book = () => {
+  return (
+    <div>
+      <h2>Title</h2>
+      <p>Genre</p>
+      <span>Year</span>
+    </div>
+  );
+};
+
+export default Book;
---
+++ src/components/books/books.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Book from "../book/book";
+
+const Books = () => {
+  return (
+    <div>
+      <h1>Books</h1>
+      <Book />
+    </div>
+  );
+};
+
+export default Books;
