ea5266b599ba6d09776018e5cbff4a98c14ad40f
Filip FICO <fitz993m@gmail.com>
2019-12-27 21:48:39 +0100

    redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/app/App";
+import store from "./components/redux/store";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById("root")
 );
