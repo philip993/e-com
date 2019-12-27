8e160a7f3b7b7c9f5aeabf51f9e7fc9e8ecd21fb
Filip FICO <fitz993m@gmail.com>
2019-12-27 12:09:49 +0100

    register form

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUserEmail = e => {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleUserPasswrod = e => {
+    setPassword(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleUserInput = () => {
+    console.log("Email:" + email);
+    console.log("Password" + password);
+  };
+
   return (
     <div>
       <h2>Register Form</h2>
+      <form onSubmit={handleUserInput}>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={handleUserEmail}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={handleUserPasswrod}
+        />
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 };
