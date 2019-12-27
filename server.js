e5f8f09ae300ad028d4ff1b8bd25e912050a34dc
Filip FICO <fitz993m@gmail.com>
2019-12-27 21:49:47 +0100

    cors policy

--- server.js
+++ server.js
@@ -13,18 +13,18 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "X-Request-With, Authorization, Origin, Accept, Content-Type"
-//     ),
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Request-With, Authorization, Origin, Accept, Content-Type"
+    ),
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use("/books", books);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on ${port}`));
