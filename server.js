02636576ae49d11d4c66f875c98a921e9eb40d51
Filip FICO <fitz993m@gmail.com>
2019-12-27 17:34:54 +0100

    basic server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require("body-parser");
+
+const books = require("./src/backend/routes/books");
+
+mongoose
+  .connect("mongodb://localhost/e-commerce", { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log("Could not connect to MongoDB"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "X-Request-With, Authorization, Origin, Accept, Content-Type"
+//     ),
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//   next();
+// });
+
+app.use("/books", books);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on ${port}`));
