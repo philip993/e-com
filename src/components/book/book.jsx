ac9af91cdc720b949dc15bd9a247d77447137736
Filip FICO <fitz993m@gmail.com>
2020-01-10 22:46:29 +0100

    addded price and quantity - frontend

--- src/components/book/book.jsx
+++ src/components/book/book.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-const Book = ({ title, writter, genre, year }) => {
+const Book = ({ title, writter, genre, year, price, quantity }) => {
   return (
     <div>
       <h3>{title}</h3>
       <p>Writter: {writter}.</p>
       <p>This book is from {genre} genre.</p>
       <span>It was published in {year}.</span>
+      <p>$ {price}</p>
+      <p>Quantity: {quantity}</p>
+      <span>
+        <button>ADD TO CART</button>
+      </span>
     </div>
   );
 };
