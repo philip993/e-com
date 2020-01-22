4b988fb535a0f659c92b580f01d108a50dbf95fa
Filip FICO <fitz993m@gmail.com>
2020-01-22 20:35:29 +0100

    bootstrap set up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/app/App";
 import store from "./components/redux/store";
 
