53e2a14421d71e9b8d6775476786daeff9d8489a
Filip FICO <fitz993m@gmail.com>
2020-02-19 20:37:05 +0100

    changes in layour of Profile

--- src/components/Profile/ProfileView.js
+++ src/components/Profile/ProfileView.js
@@ -1,29 +1,46 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Accordion, Card, ListGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 const ProfileView = ({ firstName, lastName, username, age, city, country }) => {
   return (
     <div>
-      <Table>
-        <tr>
-          <th>First Name: {firstName}</th>
-        </tr>
-        <tr>
-          <th>Last Name: {lastName}</th>
-        </tr>
-        <tr>
-          <th>Username: {username}</th>
-        </tr>
-        <tr>
-          <th>Age: {age}</th>
-        </tr>
-        <tr>
-          <th>City: {city}</th>
-        </tr>
-        <tr>
-          <th>Country: {country}</th>
-        </tr>
-      </Table>
+      <Accordion defaultActiveKey='0'>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant='link' eventKey='0'>
+              Personal Information
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey='0'>
+            <Card.Body>
+              <ListGroup>
+                <ListGroup.Item>{firstName}</ListGroup.Item>
+                <ListGroup.Item>{lastName}</ListGroup.Item>
+                <ListGroup.Item>{age} years old.</ListGroup.Item>
+                <ListGroup.Item>Username: {username}.</ListGroup.Item>
+                <ListGroup.Item>...</ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant='link' eventKey='1'>
+              Additional Information
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey='1'>
+            <Card.Body>
+              <ListGroup.Item>{city}</ListGroup.Item>
+              <ListGroup.Item>{country}</ListGroup.Item>
+              <ListGroup.Item>...</ListGroup.Item>
+              <ListGroup.Item>...</ListGroup.Item>
+              <ListGroup.Item>...</ListGroup.Item>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     </div>
   );
 };
