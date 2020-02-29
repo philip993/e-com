36fb57e5b771980f2ebd6e40b63885def80827d2
Filip FICO <fitz993m@gmail.com>
2020-02-29 13:12:40 +0100

    added button for clearing wishlist

--- src/components/Wishlist/Wishlist.js
+++ src/components/Wishlist/Wishlist.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import WishlistView from "./WishlistView";
-import { getAllWishlistItems } from "./WishlistActions";
+import { getAllWishlistItems, deleteWishlist } from "./WishlistActions";
+import { Button } from "react-bootstrap";
 
 const Wishlist = () => {
   const wish = useSelector(state => state.WishlistReducer);
@@ -11,12 +12,18 @@ const Wishlist = () => {
     dispatch(getAllWishlistItems());
   }, []);
 
+  const handleClearWihslist = () => {
+    dispatch(deleteWishlist());
+    console.log("clicked");
+  };
+
   return (
     <div>
       <h1>Wishlist</h1>
       {wish.wishItems.map(({ ...rest }) => (
         <WishlistView {...rest} />
       ))}
+      <Button onClick={handleClearWihslist}>Clear Wishlist</Button>
     </div>
   );
 };
