eea6a20d5339876635903fcf2fa0493ad95bc473
Filip FICO <fitz993m@gmail.com>
2020-03-01 13:52:18 +0100

    button for delete item

--- src/components/Wishlist/Wishlist.js
+++ src/components/Wishlist/Wishlist.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import WishlistView from "./WishlistView";
-import { getAllWishlistItems, deleteWishlist } from "./WishlistActions";
-import { Button } from "react-bootstrap";
+import {
+  getAllWishlistItems,
+  deleteWishlist,
+  deleteItemFromWishlist
+} from "./WishlistActions";
+import { Button, Table } from "react-bootstrap";
 
-const Wishlist = () => {
+const Wishlist = item => {
   const wish = useSelector(state => state.WishlistReducer);
   const dispatch = useDispatch();
 
@@ -16,12 +20,36 @@ const Wishlist = () => {
     dispatch(deleteWishlist());
   };
 
+  const handleRemoveOne = item => {
+    dispatch(deleteItemFromWishlist());
+    console.log(item);
+  };
+
   return (
     <div>
-      <h1>Wishlist</h1>
-      {wish.wishItems.map(({ ...rest }) => (
-        <WishlistView {...rest} />
-      ))}
+      <Table>
+        <tr>
+          <h1>Wishlist Items</h1>
+        </tr>
+        <thead>
+          <tr>
+            <th>Book</th>
+            <th>Price</th>
+            <th>Remove</th>
+          </tr>
+          <tbody>
+            {wish.wishItems.map(({ wishlistItemId }, index) => (
+              <tr>
+                <th>{wishlistItemId.title}</th>
+                <th>{wishlistItemId.price}</th>
+                <th>
+                  <Button onClick={handleRemoveOne.bind(this, index)}>X</Button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </thead>
+      </Table>
       <Button onClick={handleClearWihslist}>Clear Wishlist</Button>
     </div>
   );
