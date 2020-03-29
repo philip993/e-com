6a148e6a279e9f8e2c188349efbc52bc512f29f1
Filip FICO <fitz993m@gmail.com>
2020-03-29 22:01:16 +0200

    changes from Charge to Pay order

--- src/backend/Checkout/CheckoutController.js
+++ src/backend/Checkout/CheckoutController.js
@@ -5,39 +5,43 @@ exports.makePayment = async (req, res) => {
   let error;
   let status;
   try {
-    const { itemsFromCart, totalSumFromCart, token } = req.body;
+    const { itemsFromCart, totalSumFromCart, token, stripeOrderId } = req.body;
+    console.log(req.body);
 
     const customer = await stripe.customers.create({
       email: token.email,
       source: token.id
     });
-    //const idempotency_key = uuid();
-    const charge = await stripe.charges.create(
-      {
-        amount: totalSumFromCart * 100,
-        currency: "usd",
-        customer: customer.id,
-        receipt_email: token.email,
-        description: `Purchased items`,
-        metadata: {
-          data: itemsFromCart
-        },
-        shipping: {
-          name: token.card.name,
-          address: {
-            line1: token.card.address_line1,
-            line2: token.card.address_line2,
-            city: token.card.address_city,
-            country: token.card.address_country,
-            postal_code: token.card.address_zip
-          }
-        }
-      }
-      //   {
-      //     idempotency_key
-      //   }
-    );
-    console.log("Charge:", { charge });
+    // const idempotency_key = uuid();
+    // const charge = await stripe.charges.create(
+    //   {
+    //     amount: totalSumFromCart * 100,
+    //     currency: "usd",
+    //     customer: customer.id,
+    //     receipt_email: token.email,
+    //     description: `Purchased items`,
+    //     order: stripeOrderId,
+    //     shipping: {
+    //       name: token.card.name,
+    //       address: {
+    //         line1: token.card.address_line1,
+    //         line2: token.card.address_line2,
+    //         city: token.card.address_city,
+    //         country: token.card.address_country,
+    //         postal_code: token.card.address_zip
+    //       }
+    //     }
+    //   }
+    //   {
+    //     idempotency_key
+    //   }
+    // );
+
+    const payOrder = await stripe.orders.pay(stripeOrderId, {
+      customer: customer.id
+    });
+
+    console.log("Charge:", { payOrder });
     status = "success";
   } catch (error) {
     console.error("Error:", error);
