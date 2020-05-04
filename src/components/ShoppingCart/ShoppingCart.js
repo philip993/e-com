ee093d8cb3f70ba43101e3fd6a7123a79e607e86
Filip FICO <fitz993m@gmail.com>
2020-05-05 00:17:02 +0200

    reworked

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -56,25 +56,29 @@ const ShoppingCart = (item) => {
               <th>No.</th>
               <th>Book Title</th>
               <th>Price</th>
+              <th>Quantity</th>
               <th>Remove</th>
             </tr>
           </thead>
           <tbody>
-            {selectedBooks.copyOfBooksInCart.map(({ title, price }, index) => (
-              <tr>
-                <td>{index}.</td>
-                <td>{title}</td>
-                <td>{price.toFixed(2)}$</td>
-                <td>
-                  <Button
-                    variant='danger'
-                    onClick={handleRemoveOneBook.bind(this, index)}
-                  >
-                    x
-                  </Button>
-                </td>
-              </tr>
-            ))}
+            {selectedBooks.copyOfBooksInCart.map(
+              ({ title, price, quantity }, index) => (
+                <tr>
+                  <td>{index}.</td>
+                  <td>{title}</td>
+                  <td>{price.toFixed(2)}$</td>
+                  <td>{quantity}</td>
+                  <td>
+                    <Button
+                      variant='danger'
+                      onClick={handleRemoveOneBook.bind(this, index)}
+                    >
+                      x
+                    </Button>
+                  </td>
+                </tr>
+              )
+            )}
           </tbody>
         </Table>
         <Table style={{ width: "50%", margin: "auto" }}>
